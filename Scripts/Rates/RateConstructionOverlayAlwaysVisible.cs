e3443f1330b8f15fb70bf4cc046a9cca393b6a6e
mars-x24 <66588381+mars-x24@users.noreply.github.com>
2021-10-15 11:59:39 -0400

    0.1.50

---
+++ Scripts/Rates/RateConstructionOverlayAlwaysVisible.cs
@@ -0,0 +1,22 @@
+﻿namespace AtomicTorch.CBND.CoreMod.Rates
+{
+    using AtomicTorch.CBND.GameApi;
+
+    public class RateConstructionOverlayAlwaysVisible
+        : BaseRateBoolean<RateConstructionOverlayAlwaysVisible>
+    {
+        [NotLocalizable]
+        public override string Description =>
+            @"Determines if you see the construction overlay when building or relocating structures.
+              By default to true like vanilla game.
+              Change to 0 to disable overlay. You can still hold ALT to see it.";
+
+        public override string Id => "ConstructionOverlayAlwaysVisible";
+
+        public override string Name => "Construction overlay always visible";
+
+        public override bool ValueDefault => true;
+
+        public override RateVisibility Visibility => RateVisibility.Advanced;
+    }
+}
