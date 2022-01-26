fd6658cd4546a3b73d5258e5db9cbfb1e15b8b51
mars-x24 <66588381+mars-x24@users.noreply.github.com>
2022-01-26 01:10:59 -0500

    1.63.0

--- Scripts/Zones/Scripts/Special/SpawnGiantPragmiumSource.cs
+++ Scripts/Zones/Scripts/Special/SpawnGiantPragmiumSource.cs
@@ -10,9 +10,9 @@
     {
       triggers
           .Add(GetTrigger<TriggerWorldInit>())
-          .Add(GetTrigger<TriggerTimeInterval>().ConfigureForSpawn(TimeSpan.FromHours(3)));
+          .Add(GetTrigger<TriggerTimeInterval>().ConfigureForSpawn(TimeSpan.FromSeconds(30)));
 
-      spawnList.CreatePreset(interval: 10, padding: 10)
+      spawnList.CreatePreset(interval: 10, padding: 10, useSectorDensity: false, spawnAtLeastOnePerSector: true)
                .Add<ObjectMineralGiantPragmiumSource>().SetCustomPaddingWithSelf(79);
     }
   }
