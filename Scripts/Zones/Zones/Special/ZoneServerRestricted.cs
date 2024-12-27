bbb4abead6f1dd510c5258bd41a1709782576825
mars-x24 <66588381+mars-x24@users.noreply.github.com>
2024-12-26 22:11:00 -0500

    server zone

--- Scripts/Zones/Zones/Special/ZoneServerRestricted.cs
+++ Scripts/Zones/Zones/Special/ZoneServerRestricted.cs
@@ -1,4 +1,5 @@
-﻿using AtomicTorch.CBND.GameApi;
+﻿using AtomicTorch.CBND.CoreMod.Systems.Physics;
+using AtomicTorch.CBND.GameApi;
 
 namespace AtomicTorch.CBND.CoreMod.Zones
 {
@@ -13,6 +14,27 @@ namespace AtomicTorch.CBND.CoreMod.Zones
     {
       // Server restricted area
       Instance = this;
+
+      foreach (var position in ServerZoneInstance.AllPositions)
+      {
+        var tile = Server.World.GetTile(position);
+        if (!tile.IsValidTile)
+          continue;
+
+        foreach (var neighborTile in tile.EightNeighborTiles)
+        {
+          if (!ServerZoneInstance.IsContainsPosition(neighborTile.Position))
+          {
+            var physicsBody = Server.World.CreateStandalonePhysicsBody(position.ToVector2D());
+            physicsBody.AddShapeRectangle((1, 1), null, CollisionGroups.Water);
+            physicsBody.AddShapeRectangle((1, 1), null, CollisionGroups.HoverWater);
+            Server.World.AddStandalonePhysicsBody(physicsBody, Server.World.GetPhysicsSpace());
+            break;
+          }
+        }
+      }
     }
+
+
   }
 }
